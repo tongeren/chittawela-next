89599c32362cc217405dd8d9b6d198cf1cae6df2
Duncan van Tongeren <dpvantongeren@gmail.com>
2018-11-09 17:51:11 +0700

    Adding to SEO config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,17 +1,28 @@
 const pageTitle = "Chittawela - Heartfulness";
 const pageDescription = "Find yourself and heal your body, mind, and spirit in the holistic experience of Chittawela. Our one-week holiday retreat has a perfect balance providing the levels of care and comfort necessary to enjoy the well-being that comes from within.";
 const pageCanonical = "https://www.chittawela.com";
-// const facebookTitle = "Chittawela: Now is Time to Soothe Your Soul";
-// const facebookDescription = description;
+const facebookTitle = "Chittawela: Now is Time to Soothe Your Soul";
+const facebookDescription = pageDescription;
 // import facebookCard from '../../assets/images/chittawela_colour_text_under.jpg';
 
-// Set 'noindex: true' in development
 const SEO = 
 {
+    // Set 'noindex: true' in development 
     noindex: true,
+    // Google: 
     title: `${pageTitle}`,
     description: `${pageDescription}`,
-    canonical: `${pageCanonical}`
+    canonical: `${pageCanonical}`,
+    // Facebook:
+    openGraph: {
+        type: 'website',
+        locale: 'en_IE',
+        url: `${pageCanonical}`,
+        title: `${facebookTitle}`,
+        description: `${facebookDescription}`,
+        defaultImageWidth: 1200,
+        defaultImageHeight: 1200   
+    }   
 };
 
 export default SEO;
