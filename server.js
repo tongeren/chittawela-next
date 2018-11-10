b6e9387fecf5f6afa74c7f787b6fb388a3eaee7a
Duncan van Tongeren <dpvantongeren@gmail.com>
2018-11-10 21:16:15 +0700

    Fixing binding to  for Heroku build

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const favicon = require('serve-favicon');
 const path = require('path');
 
 const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.port || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -33,7 +34,7 @@ app.prepare()
             return handle(req, res);
         });
 
-        server.listen(3000, (err) => {
+        server.listen(port, (err) => {
             if (err) throw err;
             console.log('> Ready on http://localhost:3000');
         });
